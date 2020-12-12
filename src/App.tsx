fc331d16c2b70f5d972c515b7d3564a98d4e7479
Faiz Shaikh <fizi.live@gmail.com>
2020-12-13 04:34:08 +0500

    Update to many files.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import {getQuizDetails} from './services/quiz_service'; 
 import QuestionCard from './Components/QuestionCard';
-import {QuestionType} from './Types/quiz_types';
+import {QuestionType, QuizType} from './Types/quiz_types';
 
 function App() {
 
   let [quiz, setQuiz] = useState<QuestionType[]>([])
   let [currentStep, setCurrentStep] = useState(0);
+  let [score, setScore] = useState(0);
 
   useEffect(() => {
     async function fetchData(){
@@ -18,13 +19,23 @@ function App() {
     fetchData();
   }, []);
 
-  const handleSubmit = (e:React.FormEvent<EventTarget>) => {
+  const handleSubmit = (e:React.FormEvent<EventTarget>, userAns:string) => {
     e.preventDefault();
+
+    const currentQuestion:QuizType= quiz[currentStep];
+
+    console.log('correct And: ' + currentQuestion.correct_answer+ '---user Selection: ' + userAns);
+    
+    if(userAns === currentQuestion.correct_answer){
+      setScore(++score);
+    }
+
     if(currentStep !== quiz.length - 1)
       setCurrentStep(++currentStep);
       else {
-        alert('Quiz Completed!');
+        alert('You final score is: ' + score + 'out of: ' + quiz.length);
         setCurrentStep(0);
+        setScore(0);
       }
   }
 
--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {questionPropsType} from './../Types/quiz_types'
 
  const QuestionCard:React.FC<questionPropsType> = ({question, options, callback}) => {     
+    
+    let [selectedAns, setselectedAns] = useState('');  
+
+    const handleSelection = (ev: any) => {
+        setselectedAns(ev.target.value); 
+    }
+    
     return (
          <div className='question-container'>
              <div className='question'>
                  {question}
              </div>
-             <form onSubmit={callback}>
+             <form onSubmit={(e:React.FormEvent<EventTarget>) => callback(e, selectedAns)}>
                  {
                      options.map((opt:string, ind: number) => {
                          return(
@@ -16,7 +23,10 @@ import {questionPropsType} from './../Types/quiz_types'
                                    <input
                                        type='radio'
                                        name='opt'
+                                       required
                                        value={opt}
+                                       checked={selectedAns === opt}
+                                       onChange={handleSelection}
                                    />
                                    {opt}
                                 </label>
